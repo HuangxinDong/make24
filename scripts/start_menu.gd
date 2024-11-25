f2d8413b57e17d81fd342abea1b1c96b9c3823cd
dhx <donghx2002@outlook.com>
2024-11-25 19:39:05 +0800

    Fixed main.tscn

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -15,7 +15,7 @@ func _on_quit_button_mouse_entered() -> void:
 
 func _on_play_button_pressed() -> void:
 	emit_signal("game_started")
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 # Press Options Button
 func _on_options_button_pressed() -> void:
