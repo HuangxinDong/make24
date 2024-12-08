ac89270b654598210a311d5ddedf5af4d3b8d87c
dhx <donghx2002@outlook.com>
2024-12-08 23:15:17 +0800

    Optimize custom window

--- scripts/custom_window.gd
+++ scripts/custom_window.gd
@@ -1,45 +1,44 @@
 extends CharacterBody2D
 
-var draggingDistance
-var dir
-var dragging
-var newPosition = Vector2()
-
-var mouse_in = false
+signal window_closed
 
 @export var window_name := "Window"
 
+var offset = Vector2()
+var dragging = false
+var mouse_in = false
+
+
 func _ready():
+	# customize window's name
 	$NinePatchRect/Label.text = window_name
-	
-	
+
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed() && mouse_in:
-			draggingDistance = position.distance_to(get_viewport().get_mouse_position())
-			dir = (get_viewport().get_mouse_position() - position).normalized()
 			dragging = true
-			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
-		else:
+			offset = get_local_mouse_position()  # 记录偏移量
+		elif not event.is_pressed():
 			dragging = false
-			
-	elif event is InputEventMouseMotion:
-		if dragging:
-			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
+
 
 func _physics_process(delta):
 	if dragging:
-		velocity = (newPosition - position) * Vector2(30, 30)
-		move_and_slide()
-	
- # Set these two functions through the Area2D Signals!!
+		position = get_global_mouse_position() - offset  # 更新窗口位置
+
+
+ # Set these two functions through the Area2D Signals
 func mouse_entered():
 	mouse_in = true
+
 
 func mouse_exited():
 	mouse_in = false
 
+
 # close button
 func _on_close_pressed():
+	window_closed.emit()
 	get_parent().remove_child(self)
 	self.queue_free()
