2f5ea515858508cd0ce7874e40313da81fbdbe3f
dhx <donghx2002@outlook.com>
2024-12-08 23:13:57 +0800

    Fixed paus_menu and add save_data()

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -5,7 +5,11 @@ func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 func get_main_node():
-	return get_tree().root.get_child(0)
+	for child in get_tree().root.get_children():
+		if child.name == "Main":
+			return child
+		else:
+			print("Cannot find main")
 	
 # Press Resume Button
 func _on_resume_button_pressed() -> void:
@@ -17,6 +21,8 @@ func _on_resume_button_pressed() -> void:
 func _on_options_button_pressed() -> void:
 	pass # to do
 
+
 # Press Quit Button
 func _on_quit_button_pressed() -> void:
+	Database.save_data()
 	get_tree().quit()
